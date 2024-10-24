e5225fca66acc0cf36915d097872a6f9f36e259f
YouHaveToDo <zmfpdkwl2@gmail.com>
2024-10-24 14:22:54 +0900

    feat: sum 함수 구현

--- src/refactoring/utils/calculator.ts
+++ src/refactoring/utils/calculator.ts
@@ -5,3 +5,7 @@ export const multiply = (a: number, b: number) => {
 export const subtract = (a: number, b: number) => {
   return a - b;
 };
+
+export const sum = (a: number, b: number) => {
+  return a + b;
+};
